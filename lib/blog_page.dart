a467cf6c5b3b1aa6b2c4a88e6544f6a80aea5977
Ralph Burleson <ralph.burleson@posigent.com>
2022-05-30 21:43:30 +0200

    next things

--- lib/blog_page.dart
+++ lib/blog_page.dart
@@ -6,21 +6,46 @@ class BlogPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 150, 61, 61),
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 115, 61, 61),
-        title: Text(
-          'Blog Page',
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.black),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            'Blog Page',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.black),
+          ),
+          //foregroundColor: Colors.black,
+          centerTitle: true,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
         ),
-        //foregroundColor: Colors.black,
-        centerTitle: true,
-        elevation: 0,
-        iconTheme: IconThemeData(color: Colors.black),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Image.asset('images/rich.png', fit: BoxFit.cover),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Colors.black.withOpacity(0.05),
+                ),
+                height: 200,
+                width: double.infinity,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Top 10 tips to retire at 40 years old',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Text(
+                'The best tips to retire early',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ));
   }
 }
